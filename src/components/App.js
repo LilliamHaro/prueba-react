3b39db7b274e798b8f136a732a7eb47e75cf0e93
LilliamHaro <qualia950@gmail.com>
2018-03-10 11:08:18 -0500

    0-vacio

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import Calculator from './Calculator';
-import store from '../store/index';
+// import store from '../store/index';
 const App = () => (
   <div>
-    <Calculator store={store} />
+    <Calculator />
   </div>
 )
 
